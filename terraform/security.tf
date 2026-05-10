08114bb8e30d7c548d8330fccb835ca8238e1443
George-Chukhir <g.chuchir@gmail.com>
2026-05-10 17:15:08 +0200

    fix: ssh works correctly, but forgot to add security_rule for HTTP

--- terraform/security.tf
+++ terraform/security.tf
@@ -14,8 +14,19 @@ resource "azurerm_network_security_group" "nsg" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "80"
+    destination_port_range     = "22"
     source_address_prefix      = "*" // any source IP
+    destination_address_prefix = "*" 
+  }
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*" 
     destination_address_prefix = "*" 
   }
 }
