65f248ae41894863b607ba91f4d01d21d07615ae
George-Chukhir <g.chuchir@gmail.com>
2026-05-10 17:02:42 +0200

    change port of nsg

--- terraform/security.tf
+++ terraform/security.tf
@@ -14,7 +14,7 @@ resource "azurerm_network_security_group" "nsg" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "22"
+    destination_port_range     = "80"
     source_address_prefix      = "*" // any source IP
     destination_address_prefix = "*" 
   }
